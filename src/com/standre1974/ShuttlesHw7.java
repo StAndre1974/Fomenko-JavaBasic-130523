994f7d18d8c6b21408395dc356611a4380795a23
Andrii Fomenko <standre1974@gmail.com>
2023-05-13 19:34:07 +0300

    Аnalyzed variables

--- src/com/standre1974/ShuttlesHw7.java
+++ src/com/standre1974/ShuttlesHw7.java
@@ -2,6 +2,11 @@ package com.standre1974;
 
 public class ShuttlesHw7 {
     public static void main(String[] args) {
+        int numberOfShuttles = 100;
+        int[] prohibitedNumbers = {4, 9};
+        int[] shuttles = new int[numberOfShuttles];
+        int numberToCheck = 0;
+        int currentShuttleNumber = 0;
 
     }
 
