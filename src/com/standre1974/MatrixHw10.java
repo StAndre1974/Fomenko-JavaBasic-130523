4271a28377725e78c3cdf0ee1690ac3f4019964c
Andrii Fomenko <standre1974@gmail.com>
2023-05-13 20:04:59 +0300

    Added method checkInputNumberInt

--- src/com/standre1974/MatrixHw10.java
+++ src/com/standre1974/MatrixHw10.java
@@ -6,4 +6,18 @@ public class MatrixHw10 {
     public static void main(String[] args){
 
     }
+
+    //METHODS
+    public static int checkInputNumberInt(Scanner scanner) {
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int enteredNumber = scanner.nextInt();
+                if (enteredNumber >= 0) {
+                    return enteredNumber;
+                }
+            }
+            System.out.println("Enter valid number or 0 to exit");
+            scanner.nextLine();
+        }
+    }
 }
