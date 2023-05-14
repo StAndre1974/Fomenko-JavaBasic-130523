5bea31378f9266abb439854a8c405f006c7c54d4
Andrii Fomenko <standre1974@gmail.com>
2023-05-14 20:54:14 +0300

    Added method checkInputNumberInt, initialized scanner

--- src/com/standre1974/LotteryHw11.java
+++ src/com/standre1974/LotteryHw11.java
@@ -1,8 +1,27 @@
 package com.standre1974;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class LotteryHw11 {
     public static void main(String[] args){
 
 
+    }
+
+    //METHODS
+    public static int checkInputNumberInt(Scanner scanner) {
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int enteredNumber = scanner.nextInt();
+                if (enteredNumber == 0) {
+                    System.exit(0);
+                } else if (enteredNumber > 0) {
+                    return enteredNumber;
+                }
+            }
+            System.out.println("Enter valid number or 0 to exit");
+            scanner.nextLine();
+        }
     }
 }
