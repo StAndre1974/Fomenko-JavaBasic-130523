25f225ba55fa4bcf7288c2f1550dbb76f1ae0af2
Andrii Fomenko <standre1974@gmail.com>
2023-05-26 14:57:29 +0300

    For check Attemption 1 PersonHw13

--- src/com/standre1974/PersonHw13.java
+++ src/com/standre1974/PersonHw13.java
@@ -6,8 +6,8 @@ public class PersonHw13 {
     public String city;
     public long phoneNumber;
 
-    public String personInfo(){
-        return String.format("Call citizen %s %s from %s, you can call +%d", name, surname, city,phoneNumber);
+    public String personInfo() {
+        return String.format("Call citizen %s %s from %s, you can call +%d", name, surname, city, phoneNumber);
     }
 
     public static void main(String[] args) {
@@ -17,25 +17,22 @@ public class PersonHw13 {
         person1.surname = "Smith";
         person1.city = "New York";
         person1.phoneNumber = 2936729462846L;
+        System.out.println(person1.personInfo());
+
 
         PersonHw13 person2 = new PersonHw13();
         person2.name = "Jackie";
         person2.surname = "Chan";
         person2.city = "Shanghai";
         person2.phoneNumber = 12312412412L;
+        System.out.println(person2.personInfo());
+
 
         PersonHw13 person3 = new PersonHw13();
         person3.name = "Sherlock";
         person3.surname = "Holmes";
         person3.city = "London";
         person3.phoneNumber = 37742123513L;
-
-        System.out.print(person1.personInfo());
-        System.out.print("\n");
-
-        System.out.print(person2.personInfo());
-        System.out.print("\n");
-
-        System.out.print(person3.personInfo());
+        System.out.println(person3.personInfo());
     }
 }
