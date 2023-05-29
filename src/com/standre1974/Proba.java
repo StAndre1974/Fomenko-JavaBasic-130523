a263eb2fc13de48c4f7f93e8d87c958534c2199e
Andrii Fomenko <standre1974@gmail.com>
2023-05-29 16:58:58 +0300

    Rewrote the code BurgerHw14

--- src/com/standre1974/Proba.java
+++ src/com/standre1974/Proba.java
@@ -4,20 +4,17 @@ import java.util.*;
 
 public class Proba {
 
+
+
     public static void main(String[] args) {
-        int a = 1;
-        int b = 3;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-//        int temp = a;
-//        a = b;
-//        b = temp;
 
-//        valueExchange(a, b);
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-
-
+        Burger[]array = new Burger[3];
+        array[0] = new Burger();
+        array[1] = new Burger();
+        array[2] = new Burger();
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+        }
 
 
 
@@ -25,3 +22,24 @@ public class Proba {
 
 
 }
+
+class Burger {
+
+    private String burgerName = "Regular burger";
+    private int countBuns = 1;
+    private int countMeat = 1;
+    private int countCheese = 1;
+    private int countGreenery = 1;
+    private int countMayonnaise = 1;
+
+    public Burger() {
+        showComposition(burgerName, countBuns, countMeat, countCheese, countGreenery, countMayonnaise);
+    }
+
+    public static void showComposition(String burgerName, int countBuns, int countMeat, int countCheese,
+                                       int countGreenery, int countMayonnaise) {
+        System.out.printf("%s\nComposition:\nBun - %d\nMeat - %d\nCheese - %d\nGreenery - %d\nMayonnaise - %d\n\n",
+                burgerName, countBuns, countMeat, countCheese, countGreenery, countMayonnaise);
+    }
+}
+
