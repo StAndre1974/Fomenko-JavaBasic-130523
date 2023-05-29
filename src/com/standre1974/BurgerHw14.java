3043a80309ebd3955b641911dfd0ddab44b57e8e
Andrii Fomenko <standre1974@gmail.com>
2023-05-29 12:33:06 +0300

    Added variables BurgerHw14

--- src/com/standre1974/BurgerHw14.java
+++ src/com/standre1974/BurgerHw14.java
@@ -2,4 +2,12 @@ package com.standre1974;
 
 public class BurgerHw14 {
 
+    private String burgerName = "Regular burger";
+    private int countBuns = 1;
+    private int countMeat = 1;
+    private int countCheese = 1;
+    private int countGreenery = 1;
+    private int countMayonnaise = 1;
+
+
 }
