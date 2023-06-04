7aff95cf89e60f284950cf991c9e2ed903c88b4f
Andrii Fomenko <standre1974@gmail.com>
2023-06-04 20:06:09 +0300

    Init commit SmartphonesHw

---
+++ src/com/standre1974/SmartphonesHw16.java
@@ -0,0 +1,18 @@
+package com.standre1974;
+
+/*
+Зробіть інтерфейс Smartphones, який має містити методи call(), sms(), internet()
+Зробіть класи, які його імплементують: Androids, iPhones.
+Андроїди повинні також імплементувати LinuxOS, айфони повинні імплементувати iOS
+Створіть екземпляри кожного виду у мейн-класі
+ */
+
+public interface SmartphonesHw16 {
+    void call();
+    void sms();
+    void internet();
+}
+
+public interface LinuxOS{
+
+}
